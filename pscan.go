d55362672b013480daf500b9dc6c44ae9cc954e5
rossijonas <rossijonas@pm.me>
2022-03-25 09:19:52 -0300

    scan port 1-1024, no concurrency

--- pscan.go
+++ pscan.go
@@ -6,9 +6,16 @@ import (
 )
 
 func main() {
-	_, err := net.Dial("tcp", "scanme.nmap.org:80")
+	for i := 1; i <= 1024; i++ {
+		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+		conn, err := net.Dial("tcp", address)
 
-	if err == nil {
-		fmt.Println("Connection successful")
+		if err != nil {
+			// port is closed or filtered.
+			continue
+		}
+		conn.Close()
+
+		fmt.Printf("%d open\n", i)
 	}
 }
